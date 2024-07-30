c069edad498ea95f23fc2db57e882d54ed5c2281
Mathieu Renzo <mrenzo@arizona.edu>
2024-07-30 11:12:39 -0700

    Update website

--- build-site.el
+++ build-site.el
@@ -50,7 +50,7 @@
       org-html-head mr/site-html-head
       org-html-preamble mr/site-html-preamble
       org-html-postamble mr/html-postamble
-      org-export-html-date-format-string "%Y-%m-%d"
+      org-html-metadata-timestamp-format "%Y-%m-%d"
       )
 
 ;; define the publishing project
